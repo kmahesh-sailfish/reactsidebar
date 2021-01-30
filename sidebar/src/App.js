8a5d295c14fec836bc874049b25cffd7157778f3
suslavkrishna@gmail.com <suslavkrishna@gmail.com>
2021-01-30 11:41:51 +0530

    add the data

---
+++ sidebar/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+// import Button from "@material-ui/core/Button";
+import Demo from "./demo";
+
+function App() {
+  return <Demo />;
+}
+
+export default App;
